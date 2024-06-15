89024a2e35fb395b470db9afb7b16abaf3b28160
linus <linus.vaethroeder@gmail.com>
2024-06-15 16:41:01 +0200

    changed all to float

--- src/cgsr1_pkg/cgsr1_pkg/automation.py
+++ src/cgsr1_pkg/cgsr1_pkg/automation.py
@@ -13,15 +13,15 @@ class MainNode(Node):
 
 
         #Global varibles:
-        self.velocity_x = 0  #actually means turning 
-        self.velocity_y = 0  #actuall means driving forward
-        self.current_x = 0   #position in x direction
-        self.current_y = 0   # position in y direction
-        self.max_X = 100     #width of roof
-        self.max_Y = 150    #length of roof
-        self.helper = 0     
+        self.velocity_x = 0.0  #actually means turning 
+        self.velocity_y = 0.0  #actuall means driving forward
+        self.current_x = 0.0   #position in x direction
+        self.current_y = 0.0  # position in y direction
+        self.max_X = 100.0     #width of roof
+        self.max_Y = 150.0   #length of roof
+        self.helper = 0.0     
         self.movement_factor = 0.2      # 5 cm/s movement in an direction in 1/rate seconds, in cm
-        self.angle = 0      #current angle of the rover with respect to the roof
+        self.angle = 0.0      #current angle of the rover with respect to the roof
 
         self.driveSpeed = 0.4
         self.rotationSpeed = 0.8
@@ -35,7 +35,7 @@ class MainNode(Node):
         
         self.thread_main = threading.Thread(target=self.thread_main)
         self.thread_exited = False
-        self.rate_control_hz = 25
+        self.rate_control_hz = 25.0
        
         self.thread_main.start()
 
@@ -46,7 +46,7 @@ class MainNode(Node):
             while self.current_x < self.max_X:          # while not reached end of x-direction
                 while self.current_y < self.max_Y:      #while not reached end of y-direction
                     self.velocity_x = -1*self.driveSpeed                 #move down the y direction
-                    self.velocity_y = 0
+                    self.velocity_y = 0.0
                     self.current_y = self.current_y + self.movement_factor
 
                     # Publish the automation data
@@ -54,9 +54,9 @@ class MainNode(Node):
 
                     time.sleep(1 / self.rate_control_hz)
 
-                while self.current_y > 0:               #reached end, we drive back up
+                while self.current_y > 0.0:               #reached end, we drive back up
                     self.velocity_x = 1*self.driveSpeed
-                    self.velocity_y = 0
+                    self.velocity_y = 0.0
                     self.current_y = self.current_y - self.movement_factor
 
                     # Publish the automation data
@@ -64,23 +64,23 @@ class MainNode(Node):
 
                     time.sleep(1 / self.rate_control_hz)
                 while self.angle < (math.pi * 3/2):      #turning by 90 degrees
-                    self.velocity_x = 0
+                    self.velocity_x = 0.0
                     self.velocity_y = -1*self.rotationSpeed
 
                     # Publish the automation data
                     self.publish_automation(self.pub_automation, self.velocity_x, self.velocity_y)
 
                     time.sleep(1 / self.rate_control_hz)
-                while self.helper < self.movement_factor*25*6:   #drives in x direction by 30 cm (at least we hope)
+                while self.helper < self.movement_factor*25.0*6.0:   #drives in x direction by 30 cm (at least we hope)
                     self.velocity_x = 1*self.driveSpeed
-                    self.velocity_y = 0
+                    self.velocity_y = 0.0
                     self.helper = self.helper + self.movement_factor
                     self.current_x = self.current_x + self.movement_factor  #update current x value
                     self.publish_automation(self.pub_automation, self.velocity_x, self.velocity_y)
                     time.sleep(1 / self.rate_control_hz)
-                self.helper = 0
+                self.helper = 0.0
                 while self.angle > math.pi:    #turning back by 90 degrees
-                    self.velocity_x = 0
+                    self.velocity_x = 0.0
                     self.velocity_y = 1*self.rotationSpeed
 
                     # Publish the automation data
@@ -90,8 +90,8 @@ class MainNode(Node):
 
                 time.sleep(1 / self.rate_control_hz)
 
-            self.velocity_x = 0
-            self.velocity_y = 0
+            self.velocity_x = 0.0
+            self.velocity_y = 0.0
             self.publish_automation(self.pub_automation, self.velocity_x, self.velocity_y)
         
             
