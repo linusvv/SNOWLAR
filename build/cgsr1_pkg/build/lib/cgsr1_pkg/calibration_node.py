b09fac2fa37ebed103442c1f4db4c56f624dc296
linus <linus.vaethroeder@gmail.com>
2024-06-17 21:50:45 +0200

    saved

--- build/cgsr1_pkg/build/lib/cgsr1_pkg/calibration_node.py
+++ build/cgsr1_pkg/build/lib/cgsr1_pkg/calibration_node.py
@@ -10,17 +10,12 @@ import time
 imu_data_lock = threading.Lock()
 imu_data = 0.0
 
-
-
 class MotorCalibrationNode(Node):
-
-    
     def __init__(self):
         super().__init__('motor_calibration_node')
         self.pub_calib = self.create_publisher(Float32, "/olive/servo/calib/goal/position", QoSProfile(depth=10))
         self.publisher_cmd_vel = self.create_publisher(Twist, '/cmd_vel', 10)
-        self.subscription_calibrate = self.create_subscription(Twist, '/calibrate_motor', self.calibrate_motor_callback, QoSProfile(depth=10) )    #statt nächster Zeile
-        #self.srv = self.create_service(SetBool, 'calibrate_motor', self.calibrate_motor_callback)
+        self.srv = self.create_service(SetBool, 'calibrate_motor', self.calibrate_motor_callback)
         self.subscription_imu_data = self.create_subscription(
             Float32,
             '/imu_data',
@@ -28,17 +23,8 @@ class MotorCalibrationNode(Node):
             10
         )
 
-        self.temp = 0
-    def once():
-        if self.temp == 0:
-            return 1
-            self.temp = 1
-        else:
-            return 0
-        
-
-    def calibrate_motor_callback(self, msg):
-        if self.once():
+    def calibrate_motor_callback(self, request, response):
+        if request.data:
             self.get_logger().info('Calibration started...')
             
             # Rotate to -10 degrees
@@ -59,7 +45,14 @@ class MotorCalibrationNode(Node):
             self.calibrate_wheels()
 
             self.get_logger().info('Calibration completed.')
-            
+            response.success = True
+            response.message = 'Calibration successful'
+        else:
+            self.get_logger().info('Calibration not started. Received false request.')
+            response.success = False
+            response.message = 'Calibration not started'
+        return response
+
     def imu_data_callback(self, msg):
         global imu_data
         with imu_data_lock:
