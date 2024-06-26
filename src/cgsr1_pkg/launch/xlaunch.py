25ee74459a347141a115e145ac3e71c63e6d5724
= <=>
2024-06-26 19:30:46 +0200

    nice try

--- src/cgsr1_pkg/launch/xlaunch.py
+++ src/cgsr1_pkg/launch/xlaunch.py
@@ -6,9 +6,9 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='your_package_name',
-            executable='motor_turn_counter',
-            name='motor_turn_counter',
+            package='cgsr1_pkg',
+            executable='linus_automation',
+            name='linus_automation',
             output='screen',
         ),
     ])
